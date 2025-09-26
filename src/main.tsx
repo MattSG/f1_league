8b301a4c44d6407a577017930155023a3640561b
Matt Graham <matt.sg@hotmail.com>
2025-09-26 21:51:40 +0100

    Updates to use BrowserRouter
    Migrates from createBrowserRouter to BrowserRouter.
    
    This change allows for proper basename handling and simplifies
    the routing configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import TrackSelection from './pages/TrackSelection'
 import Weather from './pages/Weather'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { index: true, element: <TrackSelection /> },
-      { path: 'track', element: <TrackSelection /> },
-      { path: 'weather', element: <Weather /> },
-    ],
-  },
-], { basename: import.meta.env.BASE_URL })
+const basename = import.meta.env.BASE_URL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <StrictMode>
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<TrackSelection />} />
+          <Route path="track" element={<TrackSelection />} />
+          <Route path="weather" element={<Weather />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 )
